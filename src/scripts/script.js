39e563d2a83cc7e712295d2686bfe5ee7929133c
William Nicholson <nicho268@gmail.com>
2022-05-04 18:03:38 -0600

    initial commit

---
+++ src/scripts/script.js
@@ -0,0 +1,30 @@
+const questionNodes = document.querySelectorAll('.accordion__question')
+const textContentNodes = document.querySelectorAll('.accordion__content')
+
+questionNodes.forEach(question => {
+  question.addEventListener('click', e => {
+    if (e.target.classList.contains('active')) {
+      removeActiveClass()
+    } else {
+      removeActiveClass()
+      addActiveClass(question)
+      let el = question.nextElementSibling
+      addActiveClass(el)
+    }
+  })
+})
+
+const addActiveClass = element => {
+  element.classList.add('active')
+}
+
+const removeActiveClass = () => {
+  questionNodes.forEach(question => {
+    question.classList.remove('active')
+  })
+  textContentNodes.forEach(content => {
+    content.classList.remove('active')
+  })
+}
+
+//* If the selected question contains the class "active" then remove "active" from question and from sibling
